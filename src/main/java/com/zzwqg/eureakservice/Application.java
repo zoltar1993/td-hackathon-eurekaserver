b731aa0cec067c090650f6eb57e8a7cba7cced8c
feifan.wang <feifan.wang@tendcloud.com>
2017-05-20 16:15:11 +0800

    add eureka client

--- src/main/java/com/zzwqg/eureakservice/Application.java
+++ src/main/java/com/zzwqg/eureakservice/Application.java
@@ -2,12 +2,14 @@ package com.zzwqg.eureakservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by zoltar on 17-5-20.
  */
 
+@EnableDiscoveryClient
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
